19644695a8347cf1b40912f1fdad19e99c72aba9
VIGNESH E <romanvicky04102004@gmail.com>
2023-02-14 23:10:13 +0530

    Initial Commit

--- src/main/java/com/vignesh/pwdgen/password_generator/HelloController.java
+++ src/main/java/com/vignesh/pwdgen/password_generator/HelloController.java
@@ -1,16 +1,36 @@
 package com.vignesh.pwdgen.password_generator;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-    int count=0;
+    private Label displayPassword;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText(String.valueOf(++count));
+            private TextField lengthInput;
+    @FXML
+            private CheckBox upperCheck;
+    @FXML
+            private CheckBox lowerCheck;
+    @FXML
+    private CheckBox numberCheck;
+    @FXML
+    private CheckBox symbolCheck;
+
+    int count=0;
+
+
+    @FXML
+    protected void onGeneratePasswordClick(){
+        PasswordGenerator p = new PasswordGenerator(Integer.parseInt(lengthInput.getText()),
+                upperCheck.isSelected(),
+                lowerCheck.isSelected(),
+                numberCheck.isSelected(),
+                symbolCheck.isSelected());
+        displayPassword.setText(p.generatePassword());
 
     }
 }
--- src/main/java/com/vignesh/pwdgen/password_generator/PasswordGenerator.java
+++ src/main/java/com/vignesh/pwdgen/password_generator/PasswordGenerator.java
@@ -1,4 +1,39 @@
 package com.vignesh.pwdgen.password_generator;
 
+import java.util.Random;
+
 public class PasswordGenerator {
+    String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    String number = "1234567890";
+    String symbol = "!@#$%^&*()_+-=";
+    String content = "";
+
+    int length;
+    boolean u, l, n, s;
+
+    public PasswordGenerator(int length, boolean u, boolean l, boolean n, boolean s) {
+        this.length = length;
+        this.u = u;
+        this.l = l;
+        this.n = n;
+        this.s = s;
+    }
+
+    public String generatePassword() {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        if (u) content += upperCase;
+        if (l) content += lowerCase;
+        if (n) content += number;
+        if (s) content += symbol;
+
+        for (int i = 0; i < length; i++) {
+            sb.append(content.charAt(r.nextInt(content.length())));
+
+        }
+        return sb.toString();
+
+    }
 }
